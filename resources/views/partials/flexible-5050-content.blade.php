c8687ebb817f939fac771bf62ad96a83a7665155
Patrick Kerby <patrick@kerbydesign.com>
2019-04-08 18:05:34 -0600

    Fix order of 5050 component

--- resources/views/partials/flexible-5050-content.blade.php
+++ resources/views/partials/flexible-5050-content.blade.php
@@ -1,5 +1,5 @@
 <section class="5050 row justify-content-center">
-  <div class="col-sm-6 col-md-5 order-2 order-sm-1">
+  <div class="col-sm-6 col-md-5 order-2 order-sm-first">
     @php the_sub_field('column_1'); @endphp
   </div>
   <div class="col-sm-6 col-md-5">
