2e4b67574ca8828ea98ad12304df738e34d30cde
Patrick Kerby <patrick@kerbydesign.com>
2019-06-07 13:27:41 -0400

    Add Sponsors try 2

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -13,7 +13,7 @@ $sponsor_logo = get_field('sponsor_logo', 'option');
   @endwhile
   <div class="row full-width justify-content-center sponsors">
     <div class="col-sm-10">
-      
+      <h4>Our sponsors make our great programming possible!</h4>
       @while ( have_rows('sponsor','option') )
 
         @php the_row();
