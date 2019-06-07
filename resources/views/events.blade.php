3d60cf12feb7206993b31adc33f938bccb5376dd
Patrick Kerby <patrick@kerbydesign.com>
2019-06-07 13:24:08 -0400

    Include Sponsors on Events page

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,6 +1,9 @@
  {{--
   Template Name: Events
 --}}
+@php
+$sponsor_logo = get_field('sponsor_logo', 'option'); 
+@endphp
 
 @extends('layouts.app')
 
@@ -8,4 +11,19 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-page')
   @endwhile
+  <div class="row full-width justify-content-center sponsors">
+    <div class="col-sm-10">
+      
+      @while ( have_rows('sponsor','option') )
+
+        @php the_row();
+          $sponsor_logo = get_sub_field('sponsor_logo', 'option');
+        @endphp 
+
+        <img src="{{ $sponsor_logo }}" alt="Thanks to our sponsors!" />
+
+      @endwhile
+      
+    </div>
+  </div>
 @endsection
