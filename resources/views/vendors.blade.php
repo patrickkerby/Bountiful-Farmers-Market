d5a9a170303d9fdaf4fc7037fe0e3e03acc72f1f
Patrick Kerby <patrick@kerbydesign.com>
2019-06-14 12:44:50 -0600

    Add sponsors to Vendor Page

--- resources/views/vendors.blade.php
+++ resources/views/vendors.blade.php
@@ -22,4 +22,18 @@
       </div>
     </section>
   @endwhile
+  <div class="row full-width justify-content-center sponsors">
+    <div class="col-sm-10">
+      <h4>Our sponsors make our great programming possible!</h4>
+      @while ( have_rows('sponsor','option') )
+
+        @php the_row();
+          $sponsor_logo = get_sub_field('sponsor_logo', 'option');
+        @endphp 
+
+        <img src="{{ $sponsor_logo }}" alt="Thanks to our sponsors!" />
+
+      @endwhile
+      
+    </div>
 @endsection
