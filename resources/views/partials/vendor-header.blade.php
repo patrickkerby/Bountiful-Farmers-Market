056abb65eb302e0a38cf76fb3cc3324e2b6a6b9f
Patrick Kerby <patrick@kerbydesign.com>
2019-08-06 10:45:02 -0600

    Vendor Profiles: Responsive styles

--- resources/views/partials/vendor-header.blade.php
+++ resources/views/partials/vendor-header.blade.php
@@ -3,11 +3,11 @@
       <h2><span>Vendor Profile:</span> {!! App::title() !!}</h2>
       <ul>
         <li><span>Stall</span><br /> {{ $stall_numbers }}</li>
-        <li class="twitter"><a href="//twitter.com/{{ $vendor_social->twitter }}"><img src="@asset('images/twitter.svg')" /></a></li>
-        <li><a href="//fb.com/{{ $vendor_social->facebook }}"><img src="@asset('images/facebook.svg')" /></a></li>
-        <li><a href="//instagram.com/{{ $vendor_social->instagram }}"><img src="@asset('images/instagram.svg')" /></a></li>
-        <li><a href="mailto://{ $vendor_social->email }}"><img src="@asset('images/email.svg')" /></a></li>
-        <li class="website"><a href="//{{ $vendor_social->website }}">{{ $vendor_social->website }}</a></li>
+        <li class="twitter"><a href="//twitter.com/{{ $vendor_social->twitter }}" target="_blank"><img src="@asset('images/twitter.svg')" /></a></li>
+        <li><a href="//fb.com/{{ $vendor_social->facebook }}" target="_blank"><img src="@asset('images/facebook.svg')" /></a></li>
+        <li><a href="//instagram.com/{{ $vendor_social->instagram }}" target="_blank"><img src="@asset('images/instagram.svg')" /></a></li>
+        <li><a href="mailto://{ $vendor_social->email }}" target="_blank"><img src="@asset('images/email.svg')" /></a></li>
+        <li class="website"><a href="//{{ $vendor_social->website }}" target="_blank">{{ $vendor_social->website }}</a></li>
       </ul>
     </div>
     <div class="col-md-5 profile-img">
