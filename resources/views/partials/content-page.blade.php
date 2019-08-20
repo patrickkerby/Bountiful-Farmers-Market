c66a1f83901b2ba41422c6cb77150a7cb19e9668
Patrick Kerby <patrick@kerbydesign.com>
2019-08-20 17:36:42 -0600

    Add bootstrap basics to default page template

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,2 +1,6 @@
-@php the_content() @endphp
-{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+<section class="row justify-content-center">
+  <div class="col-sm-7">
+    @php the_content() @endphp
+    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+  </div>
+</section>
