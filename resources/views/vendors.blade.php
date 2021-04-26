00720b74d39b39552dce6854e4e0e9f8bfb75d7f
Patrick Kerby <patrick@kerbydesign.com>
2021-04-26 10:10:34 -0600

    Add sidebar widget area to vendor template

--- resources/views/vendors.blade.php
+++ resources/views/vendors.blade.php
@@ -14,14 +14,8 @@
         @include('partials.content-page')
       </div>
       <div class="col-sm-3">
-        <h2>Our vendors make our market our stand out!</h2>
-        <p>We prioritize local vendors and believe in showcasing the best of our community while increasing the availability of local products.</p>
-        <h3>More shopping hours</h3>
-        <p>With more shopping hours than any other farmers' markets in Edmonton, we bring you greater access to local products, and create more opportunities for you to make meaningful connections with local producers and makers.</p>
-        <h3>Become a vendor!</h3>
-        <p>We’re always accepting applications for new vendors.</p>
-        <p><a href="https://bountifulmarkets.com/2020-Vendor-Application.pdf" target="_blank" class="download">Vendor Application Form</a></p>
-      </div>
+        @include('partials.sidebar')
+              </div>
     </section>
   @endwhile
   @include('partials.sponsors')
